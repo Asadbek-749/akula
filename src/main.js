6c194151c01b7555c29aaaad446c82697967f3d1
Asadbek <asadbekshodiyev2002@gmail.com>
2026-01-31 21:49:56 +0500

    tayyor

--- src/main.js
+++ src/main.js
@@ -1,24 +1,35 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const boxes = document.querySelectorAll(".box");
+const restartBtn = document.getElementById("restartBtn");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let sharkIndex = null;
+let timer = null;
 
-setupCounter(document.querySelector('#counter'))
+function placeShark() {
+  boxes.forEach((box) => (box.textContent = ""));
+
+  sharkIndex = Math.floor(Math.random() * boxes.length);
+  boxes[sharkIndex].textContent = "🦈";
+
+  timer = setTimeout(() => {
+    alert("❌ Vaqt tugadi, siz yutqazdingiz!");
+    boxes[sharkIndex].textContent = "";
+  }, 2000);
+}
+
+boxes.forEach((box, index) => {
+  box.addEventListener("click", () => {
+    if (index === sharkIndex) {
+      clearTimeout(timer);
+      alert("🎉 Siz yutdingiz!");
+    } else {
+      alert("❌ Siz yutqazdingiz!");
+    }
+  });
+});
+
+restartBtn.addEventListener("click", () => {
+  clearTimeout(timer);
+  placeShark();
+});
+
+placeShark();
